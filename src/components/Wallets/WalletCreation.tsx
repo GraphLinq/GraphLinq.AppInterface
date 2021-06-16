460dafa7a1e8700c547500176b21c0677ec8636f
fafifox <fafifox@protonmail.com>
2021-06-16 20:10:45 +0200

    Create wallet modal, responsive wallet list

---
+++ src/components/Wallets/WalletCreation.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure, FormControl, FormLabel, Input } from "@chakra-ui/react";
+
+interface WalletCreationProps {}
+
+export const WalletCreation: React.FC<WalletCreationProps> = ({}) => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const createWallet = () => {
+        console.log('create wallet')
+    }
+
+    return (
+        <>
+            <Button className="bt" onClick={onOpen}>
+                Create Wallet <i className="fal fa-plus"></i>
+            </Button>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent bg="linear-gradient(to bottom, #1d1938, #15122b)" rounded="xl" size="xl">
+                    <ModalHeader color="">Create Wallet :</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <FormControl>
+                            <FormLabel>Wallet Name :</FormLabel>
+                            <Input variant="unstyled" bgColor="#090812" h="1.5rem" py="0.5rem" px="1rem" rounded="xl" type="text" />
+                        </FormControl>
+
+                        <FormControl mt={4}>
+                            <FormLabel>Password :</FormLabel>
+                            <Input variant="unstyled" bgColor="#090812" h="1.5rem" py="0.5rem" px="1rem" rounded="xl" type="password" />
+                        </FormControl>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button className="bt" mr={3} onClick={createWallet}>
+                            Create
+                        </Button>
+                        <Button variant="blackAlpha" onClick={onClose}>Cancel</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    );
+};
--- src/pages/Wallets.tsx
+++ src/pages/Wallets.tsx
@@ -1,28 +1,44 @@
 import React from "react";
-import { Box, Spacer } from "@chakra-ui/react";
+import { Text, Button, Box, Spacer, Icon, useClipboard } from "@chakra-ui/react";
+import { WalletCreation } from "../components/Wallets/WalletCreation";
+import { HiOutlineClipboardCopy } from "react-icons/hi";
 
 interface WalletsProps {}
 
 const Wallets: React.FC<WalletsProps> = ({}) => {
+    const publicKey = ""
+    const { hasCopied, onCopy } = useClipboard(publicKey)
     return (
         <>
             <h1>
                 My Wallets
-                <button className="bt">
-                    Create Wallet <i className="fal fa-plus"></i>
-                </button>
+                <WalletCreation />
             </h1>
-            <Box display="flex" alignItems="center" bg="linear-gradient(to bottom, #1d1938, #15122b)" w="full" px="8" py="6" rounded="lg">
-                <Box>
-                    WALLET_NAME
-                </Box>
-                <Box ml="12">
-                    0xeB4A6F3d8154A18aD312692eCFE4eeF8Fde66439
+            <Box display="flex" alignItems="center" w="full" px="4" py="6" rounded="lg">
+                <Box display="flex" width="25%" px={2}>Wallet name</Box>
+                <Box display="flex" width={["30%", "55%"]} px={2}>Address</Box>
+                <Spacer />
+                <Box display="flex" width={["10%", "20%"]} px={2}>
+                    Public Key
                 </Box>
                 <Spacer />
-                <Box>
-                    0.5016 ETH
+                <Box display="flex" width="10%" px={2}>Ether Amount</Box>
+            </Box>
+            <Box display="flex" justifyContent="space-between" alignItems="center" bg="linear-gradient(to bottom, #1d1938, #15122b)" w="full" px="4" py="6" rounded="lg">
+                <Box display="flex" width="25%" px={2}>WALLET 01 NAME</Box>
+                <Box display="flex" width={["30%", "55%"]} px={2}>
+                    <Text isTruncated>
+                    0xeB4A6F3d8154A18aD312692eCFE4eeF8Fde66439
+                    </Text>
                 </Box>
+                <Spacer />
+                <Box display="flex" width={["10%", "20%"]} px={2}>
+                    <Button variant="link" color="inherit" fontWeight="500" onClick={onCopy} ml={2} rightIcon={<Icon as={HiOutlineClipboardCopy} boxSize={5} />}>
+                        Copy
+                    </Button>
+                </Box>
+                <Spacer />
+                <Box display="flex" width="10%" px={2}>0.5016 ETH</Box>
             </Box>
         </>
     );
