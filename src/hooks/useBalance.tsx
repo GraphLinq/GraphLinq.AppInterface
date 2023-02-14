6205dc5a5aaf738d64797713c279fc7a4063da29
Triicky <mrtriickyx@gmail.com>
2023-02-14 23:05:29 +0100

    feat: add auto refresh balance

--- src/hooks/useBalance.tsx
+++ src/hooks/useBalance.tsx
@@ -1,31 +1,32 @@
 import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { utils } from 'ethers';
+import { utils, ethers } from 'ethers';
 import { UPDATE_BALANCE } from '../redux/actions/index';
 import { useActiveWeb3React } from '.';
 import { useTokenContract } from './useContract';
 
 export function useBalance() {
-  const { account } = useActiveWeb3React()
-
+  const { account, chainId } = useActiveWeb3React()
   const dispatch = useDispatch();
-  
+  const tokenContract = useTokenContract(process.env.REACT_APP_GRAPHLINQ_TOKEN_CONTRACT);
+  const windowObject: any = window;
+  const provider = new ethers.providers.Web3Provider(windowObject.ethereum);
+
   const balance = useSelector(state => (state as any).modals.balance);
 
-  const tokenContract = useTokenContract(process.env.REACT_APP_GRAPHLINQ_TOKEN_CONTRACT);
   const refreshBalance = useCallback(async () => {
-      if (!account || !tokenContract) {
-        return;
-      }
-      try {
-      const balanceOf = await tokenContract.balanceOf(account);
-      if (!balanceOf) return;
-  
-      const balance = parseFloat(utils.formatUnits(balanceOf, 18));
-      dispatch({ type: UPDATE_BALANCE, payload: {balance}, name: 'balance'});
-      } catch (e) { console.error(e) }
+    if (!account || !tokenContract) {
+      return;
+    }
+    try {
+      const balanceOf = (chainId === parseInt(process.env.REACT_APP_GLQ_CHAIN_ID)) ? await provider.getBalance(account) : await tokenContract.balanceOf(account);
 
+      if (!balanceOf) return;
+
+      const balance = parseFloat(utils.formatUnits(balanceOf, 18));
+      dispatch({ type: UPDATE_BALANCE, payload: { balance }, name: 'balance' });
+    } catch (e) { console.error(e) }
   }, [account, tokenContract]);
 
- return { balance, refreshBalance }
+  return { balance, refreshBalance }
 }
