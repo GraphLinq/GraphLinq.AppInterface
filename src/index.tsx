f13a968bf43985c303af76a4a8bffd6f2357ff53
John R. Bussiculo <john.graphlinq@gmail.com>
2024-11-26 12:08:46 -0500

    check if eth network is available and notify user if it is not

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createWeb3ReactRoot, Web3ReactProvider } from '@web3-react/core';
+import { createWeb3ReactRoot, Web3ReactProvider } from "@web3-react/core";
 
-import { NetworkContextName } from './constants/index';
-import getLibrary from './utils/getLibrary';
+import { NetworkContextName } from "./constants/index";
+import getLibrary from "./utils/getLibrary";
 
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react"
-import theme from './theme'
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme";
 
 const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName);
 
-if ('ethereum' in window) {
-  (window as any).ethereum.autoRefreshOnNetworkChange = true;
+// Function to check for Ethereum provider
+const checkEthereumProvider = () => {
+  const ethereum = (window as any)?.ethereum;
+  if (ethereum) {
+    // Safely access properties using optional chaining
+    ethereum.autoRefreshOnNetworkChange = true;
+    return true;
+  } else {
+    console.error("Ethereum provider (e.g., MetaMask) is not available.");
+    alert("Please install MetaMask or another Ethereum wallet provider.");
+    return false;
+  }
+};
+
+// Run the check for Ethereum provider
+if (!checkEthereumProvider()) {
+  console.log("No Ethereum provider detected.");
 }
 
 ReactDOM.render(
@@ -27,7 +42,8 @@ ReactDOM.render(
       </Web3ProviderNetwork>
     </Web3ReactProvider>
   </React.StrictMode>,
-  document.getElementById("root"));
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
